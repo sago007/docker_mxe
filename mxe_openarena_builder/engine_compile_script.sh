6cbdb10f6adf4badd57bf827b49f66766a117a9f
sago007 <poul@poulsander.com>
2016-07-13 23:39:31 +0200

    Added an OpenArena engine builder

---
+++ mxe_openarena_builder/engine_compile_script.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+set -euo pipefail
+cd /staging
+git clone git://github.com/OpenArena/engine.git
+#cp /tmp/libxmp-4.3.13/lib/libxmp.a /staging/engine/code/libs/win32/
+cd engine
+VERSION_ID=$(git log -1 --date=short --format="openarena_engine_windows_%cd_%H")
+echo $VERSION_ID
+STAGING_DIR=/staging/$VERSION_ID
+mkdir $STAGING_DIR
+#make
+#mv /staging/engine/build/release-linux-x86_64/oa_ded.x86_64 $STAGING_DIR/
+#mv /staging/engine/build/release-linux-x86_64/openarena.x86_64 $STAGING_DIR/
+#mv /staging/engine/build/release-linux-x86_64/renderer_*_x86_64.so $STAGING_DIR/
+#Windows version does not work at the moment
+cd /staging/engine
+PLATFORM=mingw32 ARCH=x86 CC=i686-w64-mingw32.static-gcc WINDRES=i686-w64-mingw32.static-windres LD=i686-w64-mingw32.static-ld PKG_CONFIG_PATH=/usr/lib/mxe/usr/i686-w64-mingw32.static/lib/pkgconfig/ make USE_CODEC_XMP=0
+mv /staging/engine/build/release-mingw32-x86/*.exe $STAGING_DIR/
+cd /staging
+zip -r $VERSION_ID $VERSION_ID
+mv $VERSION_ID.zip /data/
