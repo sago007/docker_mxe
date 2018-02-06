a5dd457eeea81e4aea02d6a734de604723c22e71
sago007 <poul@poulsander.com>
2018-02-06 19:36:10 +0100

    Always use CURL when compiling OpenArena for Windows

--- mxe_openarena_builder/engine_compile_script.sh
+++ mxe_openarena_builder/engine_compile_script.sh
@@ -20,7 +20,7 @@ mkdir $STAGING_DIR
 #mv /staging/engine/build/release-linux-x86_64/renderer_*_x86_64.so $STAGING_DIR/
 #Windows version does not work at the moment
 cd /staging/engine
-PLATFORM=mingw32 ARCH=x86 CC=i686-w64-mingw32.static-gcc WINDRES=i686-w64-mingw32.static-windres LD=i686-w64-mingw32.static-ld PKG_CONFIG_PATH=/usr/lib/mxe/usr/i686-w64-mingw32.static/lib/pkgconfig/ make USE_CODEC_XMP=0
+PLATFORM=mingw32 ARCH=x86 CC=i686-w64-mingw32.static-gcc WINDRES=i686-w64-mingw32.static-windres LD=i686-w64-mingw32.static-ld PKG_CONFIG_PATH=/usr/lib/mxe/usr/i686-w64-mingw32.static/lib/pkgconfig/ make USE_CODEC_XMP=0 USE_CURL=1
 mv /staging/engine/build/release-mingw32-x86/*.exe $STAGING_DIR/
 cd /staging
 zip -r $VERSION_ID $VERSION_ID
